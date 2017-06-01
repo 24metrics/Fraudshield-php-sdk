8e5c2bf9da8f8bbb98f533e7ed66ed9c614caeb9
Youghourta Benali <youghourta@gmail.com>
2017-06-01 15:26:32 +0100

    add the phpunit xml file

---
+++ src/Reports/ConversionReport.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Fraudshield\Reports;
+
+use DateTime;
+use Exception;
+
+class ConversionReport extends Report
+{
+    protected $count;
+    protected $page;
+    protected $sorting;
+
+    const END_POINT = "reports/conversion.json";
+
+    public function __construct($trackerId, $dateStart = null, $dateEnd = null, $timezone = null)
+    {
+        $this->trackerId = $trackerId;
+        $this->setStartDate($dateStart);
+        $this->setEndDate($dateEnd);
+        $this->timezone = $timezone;
+        $this->initializeDefaults();
+    }
+
+    protected function initializeDefaults()
+    {
+        $this->initializeDefaultDate();
+        $this->count = 50;
+        $this->page = 1;
+        $this->sorting['id']='desc';
+    }
+
+    public function setPageSize($size)
+    {
+        $this->count = $size;
+
+        return $this;
+    }
+
+    public function setPageNumber($position)
+    {
+        $this->page = $position;
+
+        return $this;
+    }
+
+    public function setSorting($column, $value)
+    {
+        $this->sorting[$column] = $value;
+
+        return $this;
+    }
+
+    protected function prepareParameters()
+    {
+        $this->parameters['tracker_id'] = $this->trackerId;
+        $this->parameters['click'] = false;
+        $this->parameters['goal'] = false;
+        $this->parameters['count'] = $this->count;
+        $this->parameters['page'] = $this->page;
+        $this->parameters['sorting'] = $this->sorting;
+
+        $this->parameters['date_start'] = $this->dateStart;
+        $this->parameters['date_end'] = $this->dateEnd;
+        $this->parameters['timezone'] = $this->timezone;
+    }
+
+}
---
+++ tests/ConversionReportTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Fraudshield\Reports\ConversionReport;
+
+class ConversionReportTest extends TestCase
+{
+    protected $cr;
+
+    protected function setUp()
+    {
+        $this->cr = new ConversionReport(1);
+    }
+
+    /** @test */
+    public function it_can_generate_report_with_just_tracker_id()
+    {
+        $this->assertInstanceOf(ConversionReport::class, $this->cr);
+    }
+
+    /** @test */
+    public function it_sets_start_end_date_to_today_if_empty()
+    {
+        $cr = new ConversionReport(1);
+        $today = date("Y-m-d");
+        $this->assertEquals($today, $cr->dateStart);
+        $this->assertEquals($today, $cr->dateEnd);
+    }
+
+    /** @test */
+    public function it_can_set_page_size()
+    {
+        $this->cr->setPageSize(100);
+        $this->assertEquals(100, $this->cr->count);
+    }
+
+    /** @test */
+    public function it_can_set_page_number()
+    {
+        $this->cr->setPageNumber(5);
+        $this->assertEquals(5, $this->cr->page);
+    }
+
+    /** @test */
+    public function it_can_set_sorting()
+    {
+        $this->cr->setSorting('id', 'asc');
+        $this->assertEquals('asc', $this->cr->sorting['id']);
+    }
+
+    /** @test */
+    public function it_can_prepare_parameters_array()
+    {
+        $this->cr->setPageSize(100);
+        $this->cr->setPageNumber(5);
+        $this->cr->setSorting('id', 'asc');
+        $this->cr->prepareParameters();
+        var_dump($this->cr->parameters);
+
+    }
+
+
+
+
+
+}
