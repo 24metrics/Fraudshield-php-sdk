98e836bb34ae623f833d991b6f533806714e61fc
Youghourta Benali <youghourta@gmail.com>
2017-06-05 15:17:35 +0100

    get rid of the api request test

--- tests/FraudReportTest.php
+++ tests/FraudReportTest.php
@@ -113,25 +113,4 @@ class FraudReportTest extends TestCase
         $end ="2017-06-01";
         $fr = new FraudReport(1, $start, $end);
     }
-
-    /** @test 
-    */
-    public function it_can_generate_partial_api_request()
-    {
-        $start = "2017-05-01";
-        $end ="2017-06-01";
-        $fr = new FraudReport(1, $start, $end);
-        $fr->addDataSource('partner')->addFilter('min_rejection_rate', 10);
-        $request = $fr->getPartialApiRequest();
-        
-        $this->assertRegexp('/reports\/fraud.json/', $request);
-        $this->assertRegexp('/2017-05-01/', $request);
-        $this->assertRegexp('/2017-06-01/', $request);
-        $this->assertRegexp('/partner/', $request);
-        $this->assertRegexp('/min_rejection_rate/', $request);
-    }
-
-
-    
-
 }
