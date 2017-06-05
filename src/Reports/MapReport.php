746319c223ca783363b53dc5cbc2b2856181fe3c
Youghourta Benali <youghourta@gmail.com>
2017-06-05 11:49:27 +0100

    add Map report and its tests

---
+++ src/Reports/MapReport.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Fraudshield\Reports;
+
+class MapReport extends Report
+{
+    protected $dataSources;
+    protected $filters;
+    protected $extraFilters;
+
+    const END_POINT = "reports/map.json";
+    const MAX_DATA_SOURCES = 4;
+    const VALID_DATA_SOURCES = ['tracker_id', 'affiliate', 'partner', 'product', 'sub_id', 'country_code'];
+    const VALID_FILTERS = ['country_code'];
+
+
+    public function __construct($dateStart = null, $dateEnd = null, $timezone = null)
+    {
+        $this->setStartDate($dateStart);
+        $this->setEndDate($dateEnd);
+        $this->timezone = $timezone;
+        $this->initializeDefaults();
+    }
+
+    protected function prepareParameters()
+    {
+        $group= [];
+        foreach ($this->dataSources as $ds) {
+            $group[] = $ds;
+        }
+        if (count($group) > 0) {
+           $this->parameters['group'] = $group; 
+        }
+
+        $search_fields= [];
+        foreach ($this->filters as $filter => $value) {
+            $search_fields[] = json_encode(["term" => $filter, "query" => $value]);
+        }
+        if (count($search_fields) > 0) {
+           $this->parameters['search_fields'] = $search_fields; 
+        }
+
+        $this->parameters['date_start'] = $this->dateStart;
+        $this->parameters['date_end'] = $this->dateEnd;
+        $this->parameters['timezone'] = $this->timezone;
+        
+        return $this;
+    }
+
+    protected function initializeDefaults()
+    {
+        $this->initializeDefaultDate();
+
+        $this->dataSources = ['country_code'];
+        $this->filters = [];
+        $this->extraFilters= [];
+    }
+
+}
---
+++ tests/MapReportTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Fraudshield\Reports\GlobalReport;
+
+class MapReportTest extends TestCase
+{
+    protected $mr;
+
+    protected function setUp()
+    {
+        $this->mr = new GlobalReport();
+    }
+
+
+    /** @test */
+    public function it_sets_start_end_date_to_today_if_empty()
+    {
+        $mr = new GlobalReport();
+        $today = date("Y-m-d");
+        $this->assertEquals($today, $mr->dateStart);
+        $this->assertEquals($today, $mr->dateEnd);
+    }
+
+    /** @test */
+    public function it_can_generate_report_with_tracker_id_and_start_date_and_end_date()
+    {
+        $start = "2017-05-01";
+        $end ="2017-06-01";
+        $mr = new GlobalReport($start, $end);
+        $this->assertEquals($start, $mr->dateStart);
+        $this->assertEquals($end, $mr->dateEnd);
+    }
+
+    /** @test */
+    public function it_can_add_data_source()
+    {
+        $this->mr->addDataSource('sub_id');
+        $this->assertContains('sub_id', $this->mr->dataSources);
+    }
+
+    /** @test */
+    public function it_can_chaine_add_data_source()
+    {
+        $this->mr->addDataSource('sub_id')->addDataSource('partner');
+        $this->assertContains('sub_id', $this->mr->dataSources);
+        $this->assertContains('partner', $this->mr->dataSources);
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_invalid_data_source()
+    {
+        $this->mr->addDataSource('some_invalid_data_source');
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_more_than_allowed_number_data_source()
+    {
+        $this->mr->addDataSource('sub_id')->addDataSource('partner')->addDataSource('affiliate')->addDataSource('product');
+    }
+
+    /** @test 
+    */
+    public function it_can_add_filters()
+    {
+        $this->mr->addFilter('tracker_id', 50);
+        $this->assertArrayHasKey('tracker_id', $this->mr->filters);
+
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_invalid_filter()
+    {
+        $this->mr->addFilter('invalid_filter', 50);
+    }
+
+    /** @test 
+    */
+    public function it_can_add_data_source_as_filter()
+    {
+        $this->mr->addDataSource('sub_id');
+        $this->mr->addFilter('sub_id', 500);        
+        $this->assertArrayHasKey('sub_id', $this->mr->filters);
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_using_invalid_date()
+    {
+        $start = "2017-May-01";
+        $end ="2017-06-01";
+        $fr = new GlobalReport($start, $end);
+    }
+
+
+}
