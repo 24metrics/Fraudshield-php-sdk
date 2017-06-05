1fdeee592d4e86276b05c8b6f656fe1b9211b847
Youghourta Benali <youghourta@gmail.com>
2017-06-05 10:12:46 +0100

    add Interval report and its tests

---
+++ src/Reports/IntervalReport.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Fraudshield\Reports;
+
+
+use Exception;
+
+class IntervalReport extends Report
+{
+    protected $chartType;
+    protected $count;
+    protected $page;
+    protected $timeInterval;
+
+    protected $dataSources;
+    protected $filters;
+    protected $extraFilters;
+
+    const END_POINT = "reports/interval.json";
+    const MAX_DATA_SOURCES = 3;
+    const VALID_DATA_SOURCES = ['tracker_id','affiliate', 'partner', 'product', 'sub_id'];
+    const VALID_FILTERS = [];
+
+    public function __construct($dateStart = null, $dateEnd = null, $timezone = null)
+    {
+        $this->setStartDate($dateStart);
+        $this->setEndDate($dateEnd);
+        $this->timezone = $timezone;
+        $this->initializeDefaults();
+    }
+
+    protected function initializeDefaults()
+    {
+        $this->initializeDefaultDate();
+
+        $this->chartType = 'conversion_rate';
+        $this->count = 25;
+        $this->page = 1;
+        $this->timeInterval = "week";
+
+        $this->dataSources = [];
+        $this->filters = ['abc'];
+        $this->extraFilters= [];
+
+        return $this;
+    }
+
+    protected function prepareParameters()
+    {
+        $this->parameters['chart_type'] = $this->chartType;
+        $this->parameters['count'] = $this->count;
+        $this->parameters['page'] = $this->page;
+        $this->parameters['time_interval'] = $this->timeInterval;
+
+        $this->parameters['date_start'] = $this->dateStart;
+        $this->parameters['date_end'] = $this->dateEnd;
+        $this->parameters['timezone'] = $this->timezone;
+    }
+
+}
---
+++ tests/IntervalReportTest.php
@@ -0,0 +1,101 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Fraudshield\Reports\IntervalReport;
+
+class IntervalReportTest extends TestCase
+{
+    protected $ir;
+
+    protected function setUp()
+    {
+        $this->ir = new IntervalReport();
+    }
+
+    /** @test */
+    public function it_can_generate_report()
+    {
+        $this->assertInstanceOf(IntervalReport::class, $this->ir);
+    }
+
+    /** @test */
+    public function it_sets_start_end_date_to_today_if_empty()
+    {
+        $fr = new IntervalReport();
+        $today = date("Y-m-d");
+        $this->assertEquals($today, $fr->dateStart);
+        $this->assertEquals($today, $fr->dateEnd);
+    }
+
+    /** @test */
+    public function it_can_generate_report_with_start_end_date()
+    {
+        $start = "2017-05-01";
+        $end ="2017-06-01";
+        $ir = new IntervalReport($start, $end);
+        $this->assertInstanceOf(IntervalReport::class, $ir);
+    }
+
+    /** @test */
+    public function it_can_chaine_add_data_source()
+    {
+        $this->ir->addDataSource('sub_id')->addDataSource('partner');
+        $this->assertContains('sub_id', $this->ir->dataSources);
+        $this->assertContains('partner', $this->ir->dataSources);
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_invalid_data_source()
+    {
+        $this->ir->addDataSource('some_invalid_data_source');
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_more_than_allowed_number_data_source()
+    {
+        $this->ir->addDataSource('sub_id')->addDataSource('partner')->addDataSource('affiliate')->addDataSource('product');
+    }
+
+    /** @test 
+    */
+    public function it_can_add_data_source_as_filter()
+    {
+        $this->ir->addDataSource('sub_id');
+        $this->ir->addFilter('sub_id', 500);        
+        $this->assertArrayHasKey('sub_id', $this->ir->filters);
+    }
+
+    /** @test 
+    */
+    public function it_can_add_filters()
+    {   
+        $this->ir->addDataSource('sub_id'); // the filters array is empty by default, so we need to add a filter (via adding a source first)
+        $this->ir->addFilter('sub_id', 1);
+        $this->assertArrayHasKey('sub_id', $this->ir->filters);
+
+    }
+
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_adding_invalid_filter()
+    {
+        $this->ir->addFilter('invalid_filter', 50);
+    }
+
+    /** @test 
+    * @expectedException Exception
+    */
+    public function it_throws_exception_when_using_invalid_date()
+    {
+        $start = "2017-May-01";
+        $end ="2017-06-01";
+        $fr = new IntervalReport($start, $end);
+    }
+
+}
