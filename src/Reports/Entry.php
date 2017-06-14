a0be3d5cb37b69f9c5664c4c1b5577407062091e
Youghourta Benali <youghourta@gmail.com>
2017-06-14 14:41:33 +0100

    add single entry report

---
+++ src/Reports/Entry.php
@@ -0,0 +1,25 @@
+<?php
+namespace Fraudshield\Reports;
+
+class Entry
+{
+
+    const END_POINT = "reports";
+
+    protected $entryType;
+    protected $entryId;
+
+    public function __construct($entryType, $entryId)
+    {
+        $this->entryType = $entryType;
+        $this->entryId = $entryId;
+    }
+
+    public function getPartialApiRequest()
+    {
+        $uri = self::END_POINT."/{$this->entryType}/{$this->entryId}?";
+
+        return $uri;
+    }
+
+}
