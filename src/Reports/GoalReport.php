24fce174eed37eaa56af803433d9e53869ddddd8
Hendrik Schultze <hendrik.schultze@innogames.com>
2017-07-25 17:19:59 +0200

    added annotations to goalreport

--- src/Reports/GoalReport.php
+++ src/Reports/GoalReport.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Fraudshield\Reports;
 
 class GoalReport extends Report
@@ -8,8 +9,26 @@ class GoalReport extends Report
     const END_POINT = "reports/goal.json";
     const MAX_DATA_SOURCES = 2;
     const VALID_DATA_SOURCES = ['affiliate', 'partner'];
-    const VALID_FILTERS = ['product', 'goal_name', 'min_conversions', 'min_goals', 'min_rejection_rate', 'max_rejection_rate', 'min_goals_reached', 'max_goals_reached', 'min_avg_goal_rate', 'max_avg_goal_rate'];
+    const VALID_FILTERS = [
+        'product',
+        'goal_name',
+        'min_conversions',
+        'min_goals',
+        'min_rejection_rate',
+        'max_rejection_rate',
+        'min_goals_reached',
+        'max_goals_reached',
+        'min_avg_goal_rate',
+        'max_avg_goal_rate'
+    ];
 
+    /**
+     * GoalReport constructor.
+     * @param int $trackerId
+     * @param string  $dateStart
+     * @param string $dateEnd
+     * @param string $timezone
+     */
     public function __construct($trackerId, $dateStart = null, $dateEnd = null, $timezone = null)
     {
         $this->trackerId = $trackerId;
@@ -28,16 +47,19 @@ class GoalReport extends Report
         $this->page = 1;
     }
 
+    /**
+     * @return GoalReport $this
+     */
     protected function prepareParameters()
     {
         $this->prepareTracker();
         $this->prepareDataSources(['product', 'goal_name']); // these two should be sent with every request
         $this->prepareSearch();
         $this->prepareTimeParameters();
-        
-        $this->parameters['sorting']['rejected']="desc";
 
-        return $this;   
+        $this->parameters['sorting']['rejected'] = "desc";
+
+        return $this;
     }
 
 }
