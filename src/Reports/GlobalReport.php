d81caf81b3ecdd6cd7f79de47cc7d506781d8871
Hendrik Schultze <hendrik.schultze@innogames.com>
2017-07-25 17:15:31 +0200

    fixed max sources and added missing annotations

--- src/Reports/GlobalReport.php
+++ src/Reports/GlobalReport.php
@@ -1,13 +1,20 @@
 <?php
+
 namespace Fraudshield\Reports;
 
 class GlobalReport extends Report
 {
     const END_POINT = "reports/global.json";
-    const MAX_DATA_SOURCES = 4;
+    const MAX_DATA_SOURCES = 6;
     const VALID_DATA_SOURCES = ['tracker_id', 'affiliate', 'partner', 'product', 'sub_id', 'country_code'];
     const VALID_FILTERS = ['tracker_id'];
 
+    /**
+     * GlobalReport constructor.
+     * @param string $dateStart
+     * @param string $dateEnd
+     * @param string $timezone
+     */
     public function __construct($dateStart = null, $dateEnd = null, $timezone = null)
     {
         $this->setStartDate($dateStart);
@@ -24,12 +31,15 @@ class GlobalReport extends Report
         $this->dataSources = ['tracker_id'];
     }
 
+    /**
+     * @return GlobalReport $this
+     */
     protected function prepareParameters()
     {
         $this->prepareDataSources();
         $this->prepareSearch();
         $this->prepareTimeParameters();
-        
+
         return $this;
     }
 }
