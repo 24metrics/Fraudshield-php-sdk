345469e8098b1eaf8b0d890d9e99b2b94ddfed06
Youghourta Benali <youghourta@gmail.com>
2017-09-19 10:26:54 +0100

    enable Conversion Scroll API to set  parameter

--- src/Reports/ConversionScrollReport.php
+++ src/Reports/ConversionScrollReport.php
@@ -5,4 +5,13 @@ namespace Fraudshield\Reports;
 class ConversionScrollReport extends ConversionReport
 {
     const END_POINT = "reports/conversion_scroll.json";
+
+    protected $usesPostRequest = true;
+
+    public function setNextPageId($id)
+    {
+        $this->parameters['next_page_id'] = $id;
+
+        return $this;
+    }
 }
